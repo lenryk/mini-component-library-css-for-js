4721031dda91c9509f387f91647647135d9a3010
lenryk <lenry101@gmail.com>
2021-10-05 21:42:42 +0100

    made progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,47 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS, SIZES } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <Wrapper as={size === "large" ? WrapperLarge : Wrapper}>
+      <Bar value={value} as={value > 99 ? BarFull : Bar} size={size} />
+    </Wrapper>
+  );
 };
 
 export default ProgressBar;
+
+const Wrapper = styled.div`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 4px;
+  background: rgba(128, 128, 128, 0.15);
+  width: 362px;
+`;
+
+const WrapperLarge = styled(Wrapper)`
+  border-radius: 8px;
+  outline: rgba(128, 128, 128, 0.15) solid 8px;
+`;
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0px 0px 4px;
+  width: ${(props) => props.value + `%`};
+  height: ${(props) => {
+    if (props.size === "small") {
+      return SIZES.small;
+    } else if (props.size === "medium") {
+      return SIZES.medium;
+    } else {
+      return SIZES.large;
+    }
+  }};
+`;
+
+const BarFull = styled(Bar)`
+  border-radius: 4px;
+`;
